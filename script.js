f68f0d0e7d395280c68cd2b815c2a33a401bd518
Almeida-91 <aplalmeida91@outlook.com>
2022-07-04 14:03:17 +0100

    Add 5 round game mode

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
 function computerPlay(){
     let play = Math.random()*100+1;
-    if (play < 33) return `rock`;
-    else if (play>=33 && play <66) return `paper`;
-    else return `scissors`;
+    if (play < 33) return `Rock`;
+    else if (play>=33 && play <66) return `Paper`;
+    else return `Scissors`;
 }
 
 function playRound(){
     let playerSelection = prompt(`Rock/Paper/Scissors`).toLowerCase();
+    playerSelection = ((playerSelection.slice(0,1)).toUpperCase()).concat(playerSelection.slice(1));
     let computerSelection = computerPlay();
+
     if (playerSelection == computerSelection){
+        console.log(`Player Selected: ${playerSelection}`);
+        console.log(`Computer Selected: ${computerSelection}`);
         return `Draw!!`;
     } else {
-        if (playerSelection == `rock`){
-                if (computerSelection == `paper`){
-                return "You Lose...";
+        console.log(`Player Selected: ${playerSelection}`);     
+        console.log(`Computer Selected: ${computerSelection}`); 
+        if (playerSelection == `Rock`){
+                if (computerSelection == `Paper`){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`;
             } else {
-                return "You Win!";
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
             }
-        } else if (playerSelection == `paper`){
-            if (computerSelection == `scissors`){
-                return "You lose...";
-            } else if(computerSelection == `rock`){
-                return "You Win!";
+        } else if (playerSelection == `Paper`){
+            if (computerSelection == `Scissors`){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            } else if(computerSelection == `Rock`){
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
             }
-        } else if (playerSelection == `scissors`){
-            if(computerSelection == `rock`){
-                return "You lose...";
+        } else if (playerSelection == `Scissors`){
+            if(computerSelection == `Rock`){
+                return `You Lose! ${computerSelection} beats ${playerSelection}`;
             }
             else if (computerSelection == `Paper`){
-                return "You Win!";
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
             }
         }
+    }
+}
+
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    let result = ""
+    for( let i = 0 ; i < 5 ; i++ ){
+        result = playRound().slice(0,7);
+        if ( result == `You Win`){
+            playerScore += 1;
+            console.log(`Player Score : ${playerScore}`);
+            console.log(`Computer Score : ${computerScore}`);
+        } else if ( result == `You Los`){
+            computerScore += 1;
+            console.log(`Player Score : ${playerScore}`);
+            console.log(`Computer Score : ${computerScore}`);
+        } else {
+            console.log(`Player Score : ${playerScore}`);
+            console.log(`Computer Score : ${computerScore}`);
+        }
+    }
+    if ( playerScore > computerScore ){
+        return "Player Wins!";
+    } else if( playerScore < computerScore){
+        return "Computer Wins!";
+    } else {
+        return "Draw!!"
     }
 }
 
